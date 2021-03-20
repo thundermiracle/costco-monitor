6588025a4d3475592e5b38259da76f5521851c16
thundermiracle <cissoliu@gmail.com>
2021-03-20 14:59:30 +0900

    fix log4js path problem

--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,14 @@
-// import { configure, getLogger } from "log4js";
-// import path from "path";
-// import { readFileSync } from "fs";
+import { configure, getLogger, Configuration, FileAppender } from "log4js";
+import path from "path";
+import { readFileSync } from "fs";
 
-// const configPath = path.join(__dirname, "../log4js_config.json");
-// const log4jsConfigString = readFileSync(configPath, { encoding: "utf8" });
-// configure(JSON.parse(log4jsConfigString));
+const configPath = path.join(__dirname, "../log4js_config.json");
+const log4jsConfigString = readFileSync(configPath, { encoding: "utf8" });
+const config = JSON.parse(log4jsConfigString) as Configuration;
 
-import { configure, getLogger } from "log4js";
-import config from "../log4js_config";
+// change filePath to absolute path
+const appendar = config.appenders.normal as FileAppender;
+appendar.filename = path.join(__dirname, "../", appendar.filename);
 
 configure(config);
 
